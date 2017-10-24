f2834d90ae0da0ecede1712b2a50fe4a96c6fa9b
Michael Ruf <m.ruf@hs-weingarten.de>
2017-10-24 13:06:24 +0200

    Initial project creation

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceWar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = new GameWindow();
+            window.Run();
+        }
+    }
+}
