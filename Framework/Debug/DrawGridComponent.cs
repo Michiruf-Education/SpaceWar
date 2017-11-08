fb1b689ef1785e015f8a893e47578fd2fdc866f0
Michael Ruf <Ruf_Michael@gmx.de>
2017-11-08 09:36:09 +0100

    Fix gitignore not ignore "[Dd]ebug" directories in the project; add 2 missing files for building due to the gitignore

---
+++ Framework/Debug/DrawGridComponent.cs
@@ -0,0 +1,12 @@
+﻿using SpaceWar.Framework.Object;
+
+namespace SpaceWar.Framework.Render {
+
+	public class DrawGridComponent : Component, RenderComponent {
+
+		public void Render() {
+			// TODO Draw a grid!
+		}
+	}
+
+}
---
+++ Framework/Debug/FrameworkDebugMode.cs
@@ -0,0 +1,10 @@
+﻿namespace SpaceWar.Framework.Debug {
+
+	public static class FrameworkDebugMode {
+
+		public static bool IsEnabled { get; set; }
+		
+		// TODO We might want to use #if DEBUG?
+	}
+
+}
