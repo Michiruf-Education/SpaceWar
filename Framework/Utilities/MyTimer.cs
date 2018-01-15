4c95b14657f324868a2bc19dd68fe2051dabf177
Michael Ruf <Ruf_Michael@gmx.de>
2018-01-15 16:07:54 +0100

    Fix a bug in the timer that initialized game objects when created by the timer callback not correctly!

--- Framework/Utilities/MyTimer.cs
+++ Framework/Utilities/MyTimer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Timers;
+using OpenTK;
 
 namespace Framework.Utilities {
 
@@ -28,7 +29,13 @@ namespace Framework.Utilities {
 			void TimerElapsed(object sender, ElapsedEventArgs args) {
 				// May perform the action on end
 				if (when == When.End) {
-					action?.Invoke();
+					// Note that we need to perform the action on the next Update() call
+					// because may created game object would not be initialized correctly!
+					void UpdateAction(object o, FrameEventArgs eventArgs) {
+						Game.Instance.Window.UpdateFrame -= UpdateAction;
+						action?.Invoke();
+					}
+					Game.Instance.Window.UpdateFrame += UpdateAction;
 				}
 
 				// Remove the callback, because else it would still get called everytime and
@@ -38,7 +45,7 @@ namespace Framework.Utilities {
 				Timer.Stop();
 			}
 
-			// Set data an start
+			// Set data at start
 			Timer.Interval = seconds * 1000f;
 			Timer.Elapsed += TimerElapsed;
 			Timer.Start();
